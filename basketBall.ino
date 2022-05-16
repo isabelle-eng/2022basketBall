f7dd4928ecab65dcda02442466fb4daea79f380b
isabelle-eng <105623446+isabelle-eng@users.noreply.github.com>
2022-05-16 17:09:33 +0900

    Update basketBall.ino

--- basketBall.ino
+++ basketBall.ino
@@ -19,7 +19,7 @@ void setup() {
  
 void loop() {
   // put your main code here, to run repeatedly:
-    long duration, distance;
+    long duration, distance;  //시간, 거리
  
     digitalWrite(TRIG, LOW);
     delayMicroseconds(2);
@@ -28,14 +28,14 @@ void loop() {
     digitalWrite(TRIG, LOW);
  
     duration = pulseIn (ECHO, HIGH);
-    distance = duration * 17 / 1000; 
+    distance = duration * 17 / 1000;  //cm변환
     Serial.println(duration ); 
     Serial.print("\nDIstance : ");
     Serial.print(distance);
     Serial.println(" Cm");
     delay(10); 
  
-    if (distance<10) {
+    if (distance<10) {  //공과 센서 사이 거리가 10cm
         digitalWrite(led, HIGH);
         digitalWrite(led2, HIGH);
         tone(speakerpin, 73, 1000);
